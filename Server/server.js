57cd080c01acdab0ccbc8c144a90fbb56a6d25ed
Alan Babu <alanbabu518@gmail.com>
2026-07-27 06:56:17 +0530

    Restrict CORS to specific origin for security

--- Server/server.js
+++ Server/server.js
@@ -12,7 +12,9 @@ connectDB();
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin : " https://movie-finder-chi-ochre.vercel.app"
+}))
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({
     extended: true,
