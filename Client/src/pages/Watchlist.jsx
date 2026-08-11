e17f4375282240989a03ed73ba435775f4fa600f
Alan Babu <alanbabu518@gmail.com>
2026-08-11 21:02:19 +0530

    Refactor Watchlist layout and table structure

--- Client/src/pages/Watchlist.jsx
+++ Client/src/pages/Watchlist.jsx
@@ -2,6 +2,7 @@ import { toast } from "react-toastify";
 import BASE_URL from "../Services/api";
 import Navbar from "../components/Navbar";
 import { useState, useEffect } from "react";
+import "./Watchlist.css";
 
 function Watchlist({ watchlist, setWatchlist }) {
 
@@ -52,59 +53,61 @@ function Watchlist({ watchlist, setWatchlist }) {
   return (
     <>
       <Navbar watchlist={watchlist} search="" setSearch={() => {}} />
-      <div className="table" style={{ color: "white", paddingTop: "100px", paddingLeft: "40px" }}>
+      <div className="watchlist-page">
         <h1 style={{ textAlign: "center", color: "red" }}>My Watchlist</h1>
-        <table
-          className="list"
-          border="1"
-          cellPadding="5"
-          style={{ width: "100%", marginTop: "20px" }}
-        >
-          <thead style={{ color: "red" }}>
-            <tr>
-              <th>Poster</th>
-              <th>Title</th>
-              <th>Genre</th>
-              <th>Rating</th>
-              <th>Overview</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {watchlist.map((movie, idx) => (
-              <tr key={movie._id || idx}>
-                <td>
-                  <img
-                    src={`https://image.tmdb.org/t/p/w200${movie.poster}`}
-                    alt=""
-                    width="70"
-                  />
-                </td>
-                <td>{movie.title}</td>
-                <td>{movie.genre}</td>
-                <td>{movie.rating}</td>
-                <td>{movie.overview}</td>
-                <td>
-                  <button
-                    onClick={() => removeMovie(movie._id)}
-                    style={{
-                      background: "#e50914",
-                      padding: 10,
-                      margin: 0,
-                      height: 40,
-                      border: "none",
-                      color: "#fff",
-                      fontWeight: "bold",
-                      cursor: "pointer",
-                    }}
-                  >
-                    Remove
-                  </button>
-                </td>
+        <div className="watchlist-table-wrapper">
+          <table
+            className="list"
+            border="1"
+            cellPadding="5"
+            style={{ width: "100%", marginTop: "20px" }}
+          >
+            <thead style={{ color: "red" }}>
+              <tr>
+                <th>Poster</th>
+                <th>Title</th>
+                <th>Genre</th>
+                <th>Rating</th>
+                <th>Overview</th>
+                <th>Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {watchlist.map((movie, idx) => (
+                <tr key={movie._id || idx}>
+                  <td>
+                    <img
+                      src={`https://image.tmdb.org/t/p/w200${movie.poster}`}
+                      alt=""
+                      width="70"
+                    />
+                  </td>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre}</td>
+                  <td>{movie.rating}</td>
+                  <td>{movie.overview}</td>
+                  <td>
+                    <button
+                      onClick={() => removeMovie(movie._id)}
+                      style={{
+                        background: "#e50914",
+                        padding: 10,
+                        margin: 0,
+                        height: 40,
+                        border: "none",
+                        color: "#fff",
+                        fontWeight: "bold",
+                        cursor: "pointer",
+                      }}
+                    >
+                      Remove
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
